373e3411010fff6ff82755c80041e01f89d40b04
Ancient Nimbus <99920424+AncientNimbus@users.noreply.github.com>
2025-05-06 17:56:42 +0100

    chore: Define logic requirements

---
+++ js/javascript.js
@@ -0,0 +1,16 @@
+// TODO: Get value from inputs
+// TODO: Save first number value
+// TODO: Save operator value
+// TODO: Save second number value
+// TODO: e-notation conversion (large value)
+// TODO: Display number
+// TODO: Single floating point support
+// TODO: Percent conversion
+// TODO: + ops
+// TODO: - ops
+// TODO: * ops
+// TODO: / ops
+// TODO: negate ops +-
+// TODO: All Clear ops
+// TODO: Clear ops
+// TODO: Keyboard support
